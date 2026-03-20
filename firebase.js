a93fee9bd745a637d8d67a74dd65b5e3b8952228
abdullahparvaiz07 <abdullahparvaiz2025@gmail.com>
2026-03-20 11:52:59 +0500

    Refactor firebase.js to use environment variables and fix authentication components

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDNxAkvuFvs-IexqwqkiUAL7O8eRM2dvNw",
-  authDomain: "fitnexaai-f4666.firebaseapp.com",
-  projectId: "fitnexaai-f4666",
-  storageBucket: "fitnexaai-f4666.firebasestorage.app",
-  messagingSenderId: "1074462133039",
-  appId: "1:1074462133039:web:57cbd591de1dc23e892fda",
-  measurementId: "G-V46RXRR5QF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase (ensure it's only initialized once in Next.js)
